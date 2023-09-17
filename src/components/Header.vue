cfe69d9900e21021d45b3fd924e887d8f5cdf54f
Afif-Ghifari <aghifari1204@gmail.com>
2023-09-17 15:18:19 +0700

    Header & Hero section

---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <button class="navbar-btn-sm" @click="toggleNavbar" v-if="showNavbarBtn"></button>
+
+    <div class="navbar-backdrop" v-if="showNavbar"></div>
+    <div class="dropbar" v-if="showNavbar">
+        <ul>
+            <div class="w-full mt-6"><div class="w-full text-right text-2xl"><button class="mr-[54px]" @click="toggleNavbar">X</button></div></div>
+            <li><a>About</a></li>
+            <li><a>Experience</a></li>
+            <li><a>Education</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Skills</a></li>
+            <li><a>Contacts</a></li>
+        </ul> 
+    </div>
+    
+    <div class="navbar-lg">
+        
+        <div class="w-1/12 hidden lg:inline">LOGO</div>
+        
+        <ul>
+            <li><a>About</a></li>
+            <li><a>Experience</a></li>
+            <li><a>Education</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Skills</a></li>
+            <li><a>Contacts</a></li>
+        </ul>
+    
+    </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showNavbar = ref(false)
+const showNavbarBtn = ref(true)
+
+const toggleNavbar = () => {
+    showNavbarBtn.value = !showNavbarBtn.value
+    showNavbar.value = !showNavbar.value
+}
+</script>
+
+<style lang="postcss" scoped>
+.navbar-btn-sm{
+    @apply fixed inline md:hidden top-8 right-8 h-14 w-14 bg-slate-200 hover:bg-slate-400 border shadow-md border-black rounded-full z-20
+}
+.dropbar{
+    @apply w-full fixed inline md:hidden h-[450px] bg-slate-200 py-5 z-20
+}
+.dropbar ul{
+    @apply flex flex-col 
+}
+.dropbar ul li{
+    @apply flex items-center w-full my-1 py-3 cursor-pointer hover:bg-white
+}
+.dropbar ul li a{
+    @apply text-base text-center mx-auto 
+}
+.navbar-backdrop{
+    @apply bg-slate-900 w-screen h-screen fixed opacity-25 z-10 inline md:hidden
+}
+
+
+.navbar-lg{
+    @apply hidden md:flex flex-row justify-between h-24 w-full py-7 px-10 bg-slate-200 border 
+    border-black mx-auto shadow-lg fixed
+}
+.navbar-lg ul{
+    @apply grid grid-cols-6 mx-auto md:w-9/12 lg:w-1/2
+}
+.navbar-lg ul li{
+    @apply flex items-center w-full cursor-pointer hover:bg-white
+}
+.navbar-lg ul li a{
+    @apply text-base text-center mx-auto 
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="hero-backdrop">
+        <div class="hero-text">
+            <h1>Muhammad Afif Al Ghifari</h1>
+            <h2>Front End Developer</h2>
+            <button class="resume-button">Download Resume</button>
+        </div>
+    </section>
+</template>
+
+<style lang="postcss" scoped>
+.hero-backdrop{
+    @apply flex items-center w-screen h-screen bg-slate-600 text-white
+}
+
+.hero-text{
+    @apply mx-auto text-center h-64 px-5
+}
+.hero-text h1{
+    @apply my-4 text-4xl sm:text-5xl
+}
+.hero-text h2{
+    @apply my-4 text-xl sm:text-3xl
+}
+
+.resume-button{
+    @apply h-14 mx-auto my-10 sm:py-8 sm:px-6 py-6 px-4 flex items-center text-xl border-2 bg-transparent border-slate-200 
+    text-slate-200 hover:text-slate-600 hover:bg-slate-200
+}
+</style>
