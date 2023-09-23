ac90bb477f9b4647d69bbef81b1bfd76274a3117
Afif-Ghifari <aghifari1204@gmail.com>
2023-09-23 20:03:54 +0700

    rename project

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/AlGhifari.github.io/',
+  base: '/AlGhifari-github/',
   plugins: [
     vue(),
   ],
