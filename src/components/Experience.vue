6db84885698e82323103b75adcd4851a5bc891c5
Afif-Ghifari <aghifari1204@gmail.com>
2023-09-25 18:37:38 +0700

    test

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -31,21 +31,21 @@
 
 <style lang="postcss" scoped>
 .experience-section{
-    @apply flex flex-col w-screen h-fit px-2 md:px-24 py-16 bg-gray-300 content-center align-middle items-center text-slate-800 
+    @apply flex flex-col w-screen h-fit px-0 md:px-24 py-16 bg-gray-300 content-center align-middle items-center text-slate-800 
 }
 .work-exp{
-    @apply my-7 h-72 md:my-0 flex-col md:flex-row flex content-center align-middle items-center
+    @apply my-14 h-fit md:h-72 md:my-0 flex-col md:flex-row flex content-center align-middle items-center
 }
 .work-timeline{
     @apply w-2/3 md:w-1/3 h-fit my-2 pb-5 md:pb-0 flex content-center align-middle items-center text-center md:border-0 border-b-2 border-slate-700 
 }
 .work-desc{
-    @apply w-3/4 md:w-2/3 h-full px-2 py-0 md:py-16 md:px-10 flex flex-col content-center align-middle border-0 md:border-l-2 border-slate-700
+    @apply w-3/4 md:w-2/3 h-full px-0 py-0 md:py-16 md:px-10 flex flex-col content-center align-middle border-0 md:border-l-2 border-slate-700
 }
 .work-desc h4{
     @apply indent-10
 }
 .work-desc p{
-    @apply pl-1 md:pl-5 lg:pl-10 pr-1 md:pr-5 lg:pr-40
+    @apply pl-0 md:pl-5 lg:pl-10 pr-0 md:pr-5 lg:pr-40
 }
 </style>
